4d86a4c73ced0ea8f6b200cf9b20a302d51171c6
Dangercoder <jarvinenemil@hotmail.com>
2022-12-20 23:44:21 +0100

    Added svelte component that uses reactivity and props

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="cljs">
-	(ns test.core)
+	(ns test.core
+	 (:require ["../components/Nested.svelte$default" :as Nested]))
 
 	(def cats [])
 
-	(js* "$: doubled = ~{}" (* 2 (count cats)))
+	(js* "$: answer = ~{}" (* 2 (count cats)))
 
 	(defn addCat []
 	    (let [new-cat-id (+ 1 (count cats))
 			  new-cat {:id 1 :name "Danger Cat"}]
 		(set! cats (conj! cats new-cat))))
+
 </script>
 
 <h1>The Famous Cats of Narnia</h1>
@@ -17,7 +19,7 @@
 	Add cat
 </button>
 
-<p>Total number of cats {doubled}</p>
+<Nested answer={answer}></Nested>
 
 <ul>
     {#each cats as cat, i}
